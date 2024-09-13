7d70d41beac8f638a32005c05260c312e538ad2b
David <darknight4123@gmail.com>
2024-09-13 09:11:18 +0300

    The hostage list is now cached for half an hour

--- src/main/java/dte/hostagechecker/services/HostageService.java
+++ src/main/java/dte/hostagechecker/services/HostageService.java
@@ -5,10 +5,15 @@ import dte.hostagechecker.hostage.Hostage;
 import dte.hostagechecker.hostage.listprovider.HostageListProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.concurrent.CompletionException;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 @Service
 public class HostageService
@@ -22,6 +27,7 @@ public class HostageService
         this.hostageListProvider = hostageListProvider;
     }
 
+    @Cacheable("hostages")
     public Collection<Hostage> getHostages()
     {
         LOGGER.info("Fetching hostage list from \"{}\".", this.hostageListProvider.getName());
@@ -36,5 +42,12 @@ public class HostageService
 
             throw new HostageFetchingException();
         }
+    }
+
+    @Scheduled(fixedDelay = 30, timeUnit = MINUTES)
+    @CacheEvict("hostages")
+    void clearHostageCache()
+    {
+        LOGGER.info("Hostages cache was cleared.");
     }
 }
