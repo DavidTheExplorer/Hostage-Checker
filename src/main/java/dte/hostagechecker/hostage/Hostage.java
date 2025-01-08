adbd5a56f01f1f809584ab793c369c355de3c178
David <darknight4123@gmail.com>
2025-01-08 10:03:58 +0200

    Added convenient shortcut methods to Hostage

--- src/main/java/dte/hostagechecker/hostage/Hostage.java
+++ src/main/java/dte/hostagechecker/hostage/Hostage.java
@@ -2,5 +2,13 @@ package dte.hostagechecker.hostage;
 
 public record Hostage(String firstName, String lastName, Age age, CaptivityStatus captivityStatus, LifeStatus lifeStatus)
 {
+    public boolean wasReturned()
+    {
+        return this.captivityStatus == CaptivityStatus.RETURNED;
+    }
 
+    public boolean isAlive()
+    {
+        return this.lifeStatus == LifeStatus.ALIVE;
+    }
 }
