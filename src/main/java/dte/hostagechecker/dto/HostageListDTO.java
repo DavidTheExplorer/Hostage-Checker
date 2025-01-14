655abe3423e5e5dde0c74405083d3a5d3ce7fd4e
David <darknight4123@gmail.com>
2025-01-14 13:36:49 +0200

    Added the "last_update" field to the response
    Grows confidence in using the API + Helps identifying problems

--- src/main/java/dte/hostagechecker/dto/HostageListDTO.java
+++ src/main/java/dte/hostagechecker/dto/HostageListDTO.java
@@ -1,20 +1,28 @@
 package dte.hostagechecker.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import dte.hostagechecker.hostage.Hostage;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Collection;
 
 import static java.util.function.Predicate.not;
 
-public record HostageListDTO(int stillInCaptive, Collection<Hostage> hostages)
+public record HostageListDTO(
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime lastUpdated,
+        int stillInCaptive,
+        Collection<Hostage> hostages)
 {
     public static HostageListDTO of(Collection<Hostage> hostages)
     {
+        LocalDateTime currentIsraelDate = LocalDateTime.now(ZoneId.of("Asia/Jerusalem"));
+
         int stillInCaptive = (int) hostages.stream()
                 .filter(not(Hostage::wasReturned))
                 .count();
 
-        return new HostageListDTO(stillInCaptive, hostages);
+        return new HostageListDTO(currentIsraelDate, stillInCaptive, hostages);
     }
 }
 
